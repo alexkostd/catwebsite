294a6da7e69b90d445424c051297cbd2cb9ef84c
alexkostd <makon122002@gmail.com>
2025-02-06 21:40:22 +0200

    nav

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// const date = (document.getElementById('date').innerHTML =
-//   new Date().getFullYear())
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear())
 
 
-// const navBtn = document.getElementById('nav-btn')
-// const navbar = document.getElementById('navbar')
-// const navClose = document.getElementById('nav-close')
+const navBtn = document.getElementById('nav-btn')
+const navbar = document.getElementById('navbar')
+const navClose = document.getElementById('nav-close')
 
 
-// navBtn.addEventListener('click', () => {
-//   navbar.classList.add('showNav')
-// })
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav')
+})
 
-// navClose.addEventListener('click', () => {
-//   navbar.classList.remove('showNav')
-// })
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav')
+})
